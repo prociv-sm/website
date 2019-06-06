9296f2b03d22b8740c05113a7b8f3b4228b7d4eb
Andrea Zago <andreacw96@gmail.com>
2019-06-06 17:44:50 +0200

    Nuxt is coming

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">
+          Benvenuto sul sito della protezione civile di Settimo Milanese
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Attualmente il sito è in sviluppo, torna in seguito per vedere le
+            modifiche!
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
