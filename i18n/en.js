230fb1d2e5f399aca4d0873623f7c50f870ea16d
Andrea Tombolato <andreacw96@gmail.com>
2022-09-07 22:28:19 +0200

    Add translation

---
+++ i18n/en.js
@@ -0,0 +1,41 @@
+export default {
+  title: 'Protezione Civile',
+  zone: 'Comune di Settimo Milanese',
+  subtitle: 'Sistema di gestione delle emergenze',
+  menu: {
+    home: 'Home',
+    login: 'Login',
+    logout: 'Logout',
+    activity: 'Attività',
+    about: 'Chi siamo',
+    vehicles: 'Veicoli'
+  },
+  activity: {
+    title: 'Attività',
+    subtitle: 'Elenco delle attività',
+    new: 'Nuova attività',
+    edit: 'Modifica attività',
+    delete: 'Elimina attività',
+    save: 'Salva attività',
+    cancel: 'Annulla',
+    type: {
+      tecnical: 'Tecnico',
+      fire: 'Incendio',
+      wather: 'Idrogeologico',
+      covid: 'Ausilio Covid-19',
+      landslide: 'Dissesto',
+      rescue: 'Soccorso',
+      event: 'Evento',
+      other: 'Altro'
+    }
+  },
+  squads: {
+    title: 'Squadre',
+    subtitle: 'Elenco delle squadre',
+    new: 'Nuova squadra',
+    edit: 'Modifica squadra',
+    delete: 'Elimina squadra',
+    save: 'Salva squadra',
+    cancel: 'Annulla'
+  }
+}
